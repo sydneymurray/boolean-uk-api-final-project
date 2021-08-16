cc1e2fb24a81b8af316f671e7010b2ef53ac678c
Sean Davison <seandavison1998@hotmail.com>
2021-08-16 16:24:45 +0100

    1.1 Added home page

---
+++ frontend/apiFinalProject/src/pages/HomePage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../components/Header";
+import HomePageImage from "../components/HomePageImage";
+
+
+
+
+
+function HomePage(){
+   
+
+    return (
+        <>
+        <Header/>
+        <HomePageImage/>
+   
+</>
+        
+    )
+}
+
+export default HomePage
